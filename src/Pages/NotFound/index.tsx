30271f6789052ba006aa11c28e7ee847be235e46
cruiserrrrrr <90287011+cruiserrrrrr@users.noreply.github.com>
2024-01-24 14:11:53 +0300

    Update index.tsx

--- src/Pages/NotFound/index.tsx
+++ src/Pages/NotFound/index.tsx
@@ -8,7 +8,7 @@ const NotFound = () => {
         <div className={styles.wrapper}>
             <div className={styles.container}>
                 <h1 className={styles.title}>Page not found...</h1>
-                <h1 className={styles.title_shadow}>Page not found...</h1>
+                <p className={styles.title_shadow}>Page not found...</p>
             </div>
             <NavItem to="/" title="market" value="Market" />
         </div>
